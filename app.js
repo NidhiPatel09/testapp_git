da699b80dffeca04c82431e99a2439eb01a25edf
Nid <nid.000059@gmail.com>
2024-03-28 12:19:05 -0400

    added ejs template

--- app.js
+++ app.js
@@ -4,8 +4,18 @@ import express from 'express'
 
 const app = express()
 
+app.use(express.static('public'))
+
+// use this line to set the view engine as ejs
+
+app.set('view-engine', 'ejs')
+
 const PORT = process.env.PORT || 8000
 
 app.listen(PORT,()=> {
     console.log(`+++++************App is listening on ${PORT}************+++++`)
 })
+
+app.get('/home', (req, res) => {
+        res.render('home.ejs')
+    })
